64129fc3dcde04bf5b504a6891d960c135c09d5e
Purvi Kanal <purvikanal92@gmail.com>
2017-01-24 17:04:45 -0500

    add a form

--- src/containers/houses-container.js
+++ src/containers/houses-container.js
@@ -10,9 +10,14 @@ export default class HousesContainer extends Component {
     this.state = {
       houses: [],
       currentHouse: 'No current house',
+      form: {
+        name: '',
+      }
     }
 
     this.selectCurrentHouse = this.selectCurrentHouse.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   selectCurrentHouse(houseName) {
@@ -22,10 +27,30 @@ export default class HousesContainer extends Component {
   }
 
   componentDidMount() {
-    $.get('http://www.anapioficeandfire.com/api/houses?pageSize=100&hasWords=true').then(response => {
+    $.get('http://www.anapioficeandfire.com/api/houses').then(response => {
       this.setState({
         houses: response
       });
+    });
+  }
+
+  handleChange(event) {
+    this.setState({
+      form: {
+        name: event.target.value,
+      }
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      houses: [...this.state.houses, {
+        name: this.state.form.name,
+      }],
+      form: {
+        name: ''
+      }
     });
   }
 
@@ -34,6 +59,14 @@ export default class HousesContainer extends Component {
       <div>
         <span style={{ color: 'red', fontSize: 20}}>Current House: {this.state.currentHouse}</span>
         <Houses selectCurrentHouse={ this.selectCurrentHouse } houses={this.state.houses} />
+        <span style={{ color: 'blue', fontSize: 20}}>{ this.state.newHouse }</span>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.form.name} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
